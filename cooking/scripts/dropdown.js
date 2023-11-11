578175de38c9b1fae53a5cf4ec9daa484f96bec0
Uri Kreindler <uri.kreindler@berkeley.edu>
2023-11-11 15:24:35 +0100

    dropdown menu done, still need to do login form appearance

---
+++ cooking/scripts/dropdown.js
@@ -0,0 +1,19 @@
+/* Inspired by a W3 Schools Tutorial for a Dropdown Menu:
+https://www.w3schools.com/howto/howto_js_dropdown.asp */
+
+var shown = false;
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function func() {
+    document.getElementById("user-dropdown").classList.toggle("show");
+    shown = !shown;
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbutton') && shown) {
+    document.getElementById("user-dropdown").classList.toggle("show");
+    shown = !shown;
+  }
+}
