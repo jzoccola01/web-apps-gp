d051166f75a09cf13ef49b5f9e2c3d16098fdcd0
Uri Kreindler <uri.kreindler@berkeley.edu>
2023-11-22 01:05:59 +0100

    meep

--- cooking/static/login_overlay.js
+++
@@ -1,7 +0,0 @@
-function loginOverlayOn() {
-    document.getElementById("login-overlay").style.display = "flex";
-}
-  
-function loginOverlayOff() {
-    document.getElementById("login-overlay").style.display = "none";
-}
