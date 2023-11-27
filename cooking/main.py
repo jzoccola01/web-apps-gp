3e0dec0d0ef837c10de229e3f3553b932f24c989
Uri Kreindler <uri.kreindler@berkeley.edu>
2023-11-27 16:30:21 +0100

    main view pretty much done

--- cooking/main.py
+++ cooking/main.py
@@ -9,9 +9,33 @@ from . import model
 bp = Blueprint("main", __name__)
 
 
+
+# For testing before recipe creation implemented
+
+class Recipe:
+    def __init__(self, title, description, servings, cook_time, photo="static/logo.png"):
+        self.title = title
+        self.description = description
+        self.servings = servings
+        self.cook_time = cook_time
+        self.photo = photo
+
+
+
 @bp.route("/")
 def index():
-    return render_template("main/index.html")
+    recipes = [Recipe("Chicken Parmesan", "A delicious chicken dish", 4, 60),
+               Recipe("Pasta Carbonara", "A delicious pasta dish", 2, 30),
+               Recipe("Steak", "A delicious steak", 1, 15),
+               Recipe("Fish", "A delicious fish", 1, 20),
+               Recipe("Burger", "A delicious burger", 1, 20),
+               Recipe("Pizza", "A delicious pizza", 1, 20),
+               Recipe("Chicken", "A delicious chicken", 1, 20),
+               Recipe("Pasta", "A delicious pasta", 1, 20),
+               Recipe("Salad", "A delicious salad", 1, 20),
+               Recipe("Soup", "A delicious soup", 1, 20),
+               Recipe("Sandwich", "A delicious sandwich", 1, 20)]
+    return render_template("main/index.html", recipes=recipes)
 
 @bp.route("/profile")
 def profile():
