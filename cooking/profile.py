bb625dbb8551820cca0caaecace3b305469a0c9c
jzoccola01 <jzoccola@calpoly.edu>
2023-12-01 13:03:22 +0100

    profile controller and html basic functionality done. Need to integrate with other files.

---
+++ cooking/profile.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, request, url_for, redirect, abort
+from . import model
+from . import db
+
+bp = Blueprint("profile", __name__)
+
+# TODO: Add flask_login's @login_required decorator here
+# TODO: Ensure that a single user cannot enter a bookmark for the same recipe twice.
+@bp.route('/profile/<int:user_id>')
+def get_profile(user_id):
+    user = db.session.get(model.User, user_id)
+    if not user:
+        abort(404, "User with id {} doesn't exist.".format(user_id))
+    return render_template('main/profile.html', user=user)
