c78b7056e1a968d7f56119b2470f342e7a866027
alecmorrison <alecmorrison@umail.ucsb.edu>
2023-12-10 15:41:12 +0100

    am - PROFILE PAGE styling... added templates/css files for recipe previews on profile page and added controller for profile page bookmark redirect

--- cooking/controllers/main.py
+++ cooking/controllers/main.py
@@ -74,6 +74,54 @@ def bookmark():
 
     return render_template("main/index.html", recipes=recipes, bookmarks=bookmarks, sort=sort_op, category=category, search=search, login=False, signup=False)
 
+@bp.route("/bookmark_profile", methods=["POST"])
+@flask_login.login_required
+def bookmark_profile():
+    recipe_id = request.form.get("recipe-id")
+    profile_user_id = request.form.get("profile-user-id")
+    user_id = flask_login.current_user.id
+    query = db.select(model.Bookmark).where(model.Bookmark.recipe_id == recipe_id).where(model.Bookmark.user_id == user_id)
+    bookmark = db.session.execute(query).scalar_one_or_none()
+    if bookmark:
+        db.session.delete(bookmark)
+    else:
+        new_bookmark = model.Bookmark(user_id=user_id, recipe_id=recipe_id)
+        db.session.add(new_bookmark)
+    db.session.commit()
+
+    category = request.form.get("category")
+    sort_op = request.form.get("sort")
+    search = request.form.get("search")
+
+    new_recipes = []
+    if sort_op == "option1":
+        # Query to order by the average rating of each recipe
+        query = db.select(model.Recipe).join(model.Rating, isouter=True).group_by(model.Recipe.id).order_by(db.func.avg(model.Rating.rating).desc())
+        new_recipes = db.session.execute(query).scalars().all()
+
+    elif sort_op == "option2":
+        # Query to get the number of ratings for each recipe and order by that
+        query = db.select(model.Recipe).join(model.Rating, isouter=True).group_by(model.Recipe.id).order_by(db.func.count(model.Rating.rating).desc())
+        new_recipes = db.session.execute(query).scalars().all()
+    elif sort_op == "option3":
+        # Query to order by the timestamp of each recipe
+        query = db.select(model.Recipe).order_by(model.Recipe.timestamp.desc())
+        new_recipes = db.session.execute(query).scalars().all()
+
+    if search == "":
+        recipes = new_recipes
+    else:
+        recipes = [r for r in new_recipes if search.lower() in r.title.lower() or search.lower() in r.description.lower() or search.lower() in r.category.lower() or search.lower() in r.user.username.lower() or any(search.lower() in i.ingredient.name.lower() for i in r.quantified_ingredients)]
+
+    bookmarks = []
+    if flask_login.current_user.is_authenticated:
+        query = db.select(model.Bookmark).where(model.Bookmark.user_id == flask_login.current_user.id)
+        bookmarks = db.session.execute(query).scalars().all()
+
+
+    return redirect(url_for("profile.get_profile", user_id = profile_user_id))
+
+
 @bp.route('/sort', methods=["POST"])
 def sort():
     sort_op = request.form.get("sort")
